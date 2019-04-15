60f70973dcf7331e1f6ee1bd05d191eaa21ba9fa
ysnnis <yannis_123@live.com>
2019-04-16 00:01:31 +0800

    modify

---
+++ src/TravelApp.Web.Admin/Controllers/BrowseController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using TravelApp.Controllers;
+using TravelApp.Travel;
+using TravelApp.Travel.Trips.Dtos;
+
+namespace TravelApp.Web.Admin.Controllers
+{
+    public class BrowseController : TravelAppControllerBase
+    {
+        private readonly ITripAppService _tipAppService;
+
+        public BrowseController(ITripAppService tripAppService)
+        {
+            _tipAppService = tripAppService;
+        }
+        public async Task<IActionResult> Index(int id)
+        {
+            TripDto trip = await _tipAppService.GetById(new Abp.Application.Services.Dto.EntityDto<int>()
+            {
+                Id = id
+            });
+            if (trip == null || string.IsNullOrEmpty(trip.TripDesc))
+            {
+                return Content("行程不存在");
+            }
+            return View(trip);
+        }
+    }
+}
